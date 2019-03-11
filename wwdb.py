5133a7b60411cb35cd74a132d0c9234338a95da5
Alexcoratt <rewq20022002@gmail.com>
2019-03-11 18:52:07 +0300

    Подправил базу данных и улучшил прогу (добавил работу с юзерами)

--- wwdb.py
+++ wwdb.py
@@ -1,28 +1,75 @@
 import sqlite3
 
 
-class DBFWorking:
+class DBUWorking:
+    def __init__(self, login, password, db_name='users.db'):
+        self.connection = sqlite3.connect(db_name, check_same_thread=False)
+        self.access = False
+        self.login = login
+        self.password = password
+        print(self.sign_in())
 
-    def __init__(self, name='photos.db'):
-        self.connection = sqlite3.connect(name, check_same_thread=False)
+    def is_login_created(self, login):
+        cursor = self.connection.cursor()
+        cursor.execute('SELECT * FROM users WHERE login = ?', (login,))
+        login = cursor.fetchone()
+        cursor.close()
+        return bool(login)
 
     def get_conn(self):
         return self.connection
 
-    def add_photo(self, photo_id, filename):
-        cursor = self.connection.cursor()
-        cursor.execute('''INSERT INTO photos
-                        (photo_id, photo) 
-                        VALUES (?,?)''', (str(photo_id), filename))
-        cursor.close()
-        self.connection.commit()
+    def sign_in(self):
+        if self.is_login_created(self.login):
+            cursor = self.connection.cursor()
+            cursor.execute('SELECT password FROM users WHERE login = ?', (self.login,))
+            base_password = str(cursor.fetchone())[2:-3]
+            cursor.close()
+            if base_password == self.password:
+                self.access = True
+                return 'successful'
+            else:
+                return 'wrong password'
+        else:
+            return 'you should sign up'
 
-    def get_photo(self, photo_id):
+    def sign_up(self):
         cursor = self.connection.cursor()
-        cursor.execute("SELECT photo FROM photos WHERE photo_id = ?", (str(photo_id),))
-        row = cursor.fetchone()
-        cursor.close()
-        return row
+        try:
+            cursor.execute('INSERT INTO users (login, password) VALUES (?,?)', (self.login, self.password))
+            self.connection.commit()
+            cursor.close()
+            return 'successful'
+        except sqlite3.IntegrityError:
+            cursor.close()
+            return 'this login has already been created'
 
     def __del__(self):
         self.connection.close()
+
+
+class DBFWorking:
+
+    def __init__(self, login, db_name='users.db'):
+        self.connection = sqlite3.connect(db_name, check_same_thread=False)
+        self.login = login
+
+    def get_conn(self):
+        return self.connection
+
+    def add_photo(self, filename):
+        cursor = self.connection.cursor()
+        cursor.execute('INSERT INTO photos (login, photo) VALUES (?,?)', (self.login, filename))
+        cursor.close()
+        self.connection.commit()
+
+    def get_photos(self):
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT photo FROM photos WHERE login = ?", (self.login,))
+        names = cursor.fetchall()
+        cursor.close()
+        return [str(name)[2:-3] for name in names]
+
+    def __del__(self):
+        self.connection.close()
+       
