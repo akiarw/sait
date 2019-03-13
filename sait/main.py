cd3d60fda942d0f306b87acee1d1f06dbfd002a6
Mark <38960846+margis902@users.noreply.github.com>
2019-03-13 14:58:45 +0300

    пацаны, мне страшно

---
+++ sait/main.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return "Привет, Яндекс!"
+
+@app.route('/countdown')
+def countdown():
+    countdown_list = [str(x) for x in range(10, 0, -1)]
+    countdown_list.append('Пуск!')
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
