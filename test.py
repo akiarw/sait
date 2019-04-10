9daaf863f8b037ba7aeb0041f80132dd7783a555
Yandex Licey <yanlic@lstu2.edu>
2019-04-10 18:05:12 +0300

    Далеко еще

--- test.py
+++ test.py
@@ -1,131 +1,10 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, session
 from flask_wtf import FlaskForm
 from wtforms import *
 from wtforms.validators import DataRequired
 from wwdb import DBWorking
 import os
-from PIL import Image
-import sys
-
-
-class Filters:
-
-    def __init__(self, image):
-        self.image = image
-        self.size_x, self.size_y = self.image.size
-        self.matrix = self.image.load()
-
-    def negative(self):
-        self.matrix = self.image.load()
-        for x in range(self.size_x):
-            for y in range(self.size_y):
-                self.matrix[x, y] = tuple(map(lambda cl: 255 - cl, self.matrix[x, y]))
-
-    def full_monochrome(self, power=350):
-        self.matrix = self.image.load()
-        for x in range(self.size_x):
-            for y in range(self.size_y):
-                self.matrix[x, y] = (255, 255, 255) if sum(self.matrix[x, y]) > power else (0, 0, 0)
-
-    def monochrome(self):
-        self.matrix = self.image.load()
-        for x in range(self.size_x):
-            for y in range(self.size_y):
-                med = sum(self.matrix[x, y]) // 3
-                self.matrix[x, y] = med, med, med
-
-    def sepia(self, koeff=30):
-        self.matrix = self.image.load()
-        for x in range(self.size_x):
-            for y in range(self.size_y):
-                self.matrix[x, y] = tuple(map(lambda cl, i: cl + koeff * i, self.matrix[x, y], range(2, -1, -1)))
-
-    def anagliph(self, delta=10):
-        self.matrix = self.image.load()
-        for x in range(self.size_x - delta - 1, -1, -1):
-            for y in range(self.size_y):
-                r, g, b = self.matrix[x, y]
-                r1, g1, b1 = self.matrix[x + delta, y]
-                self.matrix[x + delta, y] = r, g1, b1
-
-    def bright(self, koeff=10):
-        self.matrix = self.image.load()
-        for x in range(self.size_x):
-            for y in range(self.size_y):
-                self.matrix[x, y] = tuple(
-                    map(lambda cl: 255 if cl + koeff > 255 else 0 if cl + koeff < 0 else cl + koeff, self.matrix[x, y]))
-
-    def mosaic(self, lup=(0, 0), rdwn=None, power=30):
-        if not rdwn:
-            rdwn = (self.size_x, self.size_y)
-        for i in range(lup[1], rdwn[1], power * 2):
-            self.move_fragment((lup[0], i), (rdwn[0], i + power), (-1) ** i * power)
-        for i in range(lup[0], rdwn[0], power * 2):
-            self.move_fragment((i + power, lup[1] - power), (i + power * 2, rdwn[1] - power), delta_y=(-1) ** i * power)
-
-
-class Shaping:
-
-    def __init__(self, image):
-        self.image = image
-        self.size_x, self.size_y = self.image.size
-        self.sub_image = None
-
-    def resize(self, x, y=None, is_perc=False):
-        if is_perc:
-            if y:
-                x *= self.size_x // 100
-                y *= self.size_y // 100
-            else:
-                x = (x[0] * self.size_x // 100, x[1] * self.size_y // 100)
-        if y:
-            self.image = self.image.resize((x, y))
-            self.size_x, self.size_y = x, y
-        else:
-            self.image = self.image.resize(x)
-            self.size_x, self.size_y = x
-
-    def crop(self, lup, rdwn, is_perc=False):
-        if is_perc:
-            lup = (lup[0] * self.size_x // 100, lup[1] * self.size_y // 100)
-            rdwn = (rdwn[0] * self.size_x // 100, rdwn[1] * self.size_y // 100)
-        self.image = self.image.crop(lup + rdwn)
-        self.size_x, self.size_y = self.image.size
-
-    def move_fragment(self, lup, rdwn, delta_x=0, delta_y=0):
-        self.sub_image = Processing(self.image)
-        self.sub_image.crop(lup, rdwn)
-        self.sub_image.paste_image((lup[0] + delta_x, lup[1] + delta_y), self)
-
-    def paste_image(self, pos, screen=None):
-        if screen:
-            screen.image.paste(self.image, pos)
-        else:
-            sys.exit("I don't know name of screen you mean(((")
-
-    def rotate(self, angle):
-        self.image = self.image.rotate(angle)
-
-
-class Processing(Filters, Shaping):
-
-    def __init__(self, image=None):
-        self.open_image(image)
-        super().__init__(self.image)
-
-    def open_image(self, image):
-        if type(image) == str:
-            self.image = Image.open(image)
-        elif image:
-            self.image = image
-        else:
-            sys.exit("I don't know name of image you need(((")
-
-    def save(self, res_file='res.jpg'):
-        try:
-            self.image.save(res_file)
-        except ValueError:
-            self.image.save(res_file + '.jpg')
+from processing import Processing
 
 
 class MainForm(FlaskForm):
@@ -135,7 +14,7 @@ class MainForm(FlaskForm):
     my_acc = SubmitField('Моя коллекция')
     go_to_step3 = SubmitField('Перейти к следующему шагу')
     sepia = SubmitField('Сепия')
-    anagliph = SubmitField('Анаглиф ')
+    anagliph = SubmitField('Анаглиф')
     mosaic = SubmitField('Мозайка')
     monochrome = SubmitField('Монохром')
     full_monochrome = SubmitField('HARD Монохром')
@@ -195,8 +74,7 @@ class Server:
             red = redirection()
             if red:
                 return red
-            return render_template('sign.html', form=main_form, log_form=log_form, user=user,
-                                   status=res if type(res) == str else '')
+            return render_template('sign.html', form=main_form, user=user, status=res if type(res) == str else '')
 
         @app.route('/editor', methods=["GET", "POST"])
         def editor():
@@ -217,7 +95,7 @@ class Server:
 
             if red:
                 return red
-            return render_template('editor.html', form=main_form, user=user, edit_form=edit_form)
+            return render_template('editor.html', form=main_form, user=user)
 
         @app.route('/edit_step2', methods=['GET', 'POST'])
         def step2():
@@ -226,7 +104,7 @@ class Server:
             edit_form = EditForm()
             if red:
                 return red
-            return render_template('edit_step2.html', form=main_form, edit_form=edit_form)
+            return render_template('edit_step2.html', form=main_form)
 
         @app.route('/edit_step3', methods=['GET', 'POST'])
         def step3():
@@ -272,7 +150,7 @@ class Server:
                         DBWorking(nickname).save_im_info(name_ph)
                     except NameError:
                         return redirect('/sign')
-            return render_template('edit_step3.html', form=main_form, edit_form=edit_form, src=way)
+            return render_template('edit_step3.html', form=main_form, src=way)
 
         @app.route('/account', methods=['GET', 'POST'])
         def account():
@@ -330,7 +208,7 @@ class Server:
                     status = user.sign_up(pwd)
                     if status == 'successful':
                         MainForm.authorise = SubmitField('Выход')
-                        return redirect('/account')  # сами введите нужный адрес
+                        return redirect('/account')
                     return status
                 return 'different passwords'
 
